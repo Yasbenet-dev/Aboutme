9b166b61c72d7f5030a850d9ca05cb14dffac38e
Yasbenet-dev <joseu.dev@gmail.com>
2025-04-29 19:53:38 -0400

    public version 1.0

--- functionality/functions.js
+++ functionality/functions.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const text = "Hello, I'm Jose Ulloa"; // Texto a escribir
+    const text = "Hello, I'm Jose Ulloa";
     let index = 0;
-    const speed = 100; // Velocidad de escritura en milisegundos
-    const typingText = document.getElementById("typing-text"); // Seleccionamos el elemento
+    const speed = 100;
+    const typingText = document.getElementById("typing-text"); 
 
     function typeWriter() {
-        if (index < text.length) { // Si aún hay letras por escribir
-            typingText.innerHTML += text.charAt(index); // Agrega la letra actual
-            index++; // Pasa a la siguiente letra
-            setTimeout(typeWriter, speed); // Vuelve a llamar la función después del tiempo definido
+        if (index < text.length) { 
+            typingText.innerHTML += text.charAt(index); 
+            index++; 
+            setTimeout(typeWriter, speed); 
         }
     }
-    typingText.classList.add("typing"); // Agrega la clase CSS para el cursor
-    typeWriter(); // Inicia la animación
+    typingText.classList.add("typing"); 
+    typeWriter(); 
 });
